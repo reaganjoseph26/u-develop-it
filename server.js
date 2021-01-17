8272adce31e1add25cbf19f8970622f0bd2de098
Racer <reaganjoseph26@yahoo.com>
2021-01-17 09:58:55 -0500

    connection to SQLite database created

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//code connects the applicaiton to the SQLite database. 
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
 
 
 
@@ -14,6 +25,9 @@ app.use((req, res) => {
     res.status(404).end();
   });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  //Ensure Express.js(server) doesnt start before the connecttion to database has been estabished by wrapping the express.server connection in an event handler.
+  db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });

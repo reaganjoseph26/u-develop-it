345087949a61239ba3521cf7685f9f871cff4a91
Racer <reaganjoseph26@yahoo.com>
2021-01-18 18:55:53 -0500

    get and delete routes for parties created

--- server.js
+++ server.js
@@ -112,6 +112,54 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+//route to get all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+//get route to get a single party 
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+//route to be able to delete a party 
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ message: 'successfully deleted', changes: this.changes });
+    });
+});
+
 
 // Default response for any other request(Not Found) Catch all
   // route to handle user requests that aren't supported by the app,

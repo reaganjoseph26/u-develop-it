048e8569ef5974cb52a3f1555eb1f35b8f344e69
Racer <reaganjoseph26@yahoo.com>
2021-01-17 19:36:29 -0500

    post route added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
+
 
 
 // Express middleware
@@ -70,24 +72,40 @@ app.delete("/api/candidate/:id", (req,res)=>{
             changes: this.changes
             //this.changes verifies if any rows were deleted
         });
-      });
-})
+    });
+});
 
 
 //create a query for create operation
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
 
-  // Default response for any other request(Not Found) Catch all
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
+
+// Default response for any other request(Not Found) Catch all
   // route to handle user requests that aren't supported by the app,
 app.use((req, res) => {
     res.status(404).end();

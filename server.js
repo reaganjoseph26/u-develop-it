df7bc7327834d235a2b610b1dd7a7deb347bb86b
Racer <reaganjoseph26@yahoo.com>
2021-01-18 22:47:51 -0500

    Modularized Database and apiRoutes

--- server.js
+++ server.js
@@ -1,204 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const sqlite3 = require('sqlite3').verbose();
-const inputCheck = require('./utils/inputCheck');
-
-
+const db = require('./db/database');
+const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-//code connects the applicaiton to the SQLite database. 
-const db = new sqlite3.Database('./db/election.db', err => {
-    if (err) {
-      return console.error(err.message);
-    }
-  
-    console.log('Connected to the election database.');
-  });
-
-// return all data from the candidates table
-app.get("/api/candidates", (req, res) => {
-    const sql = `SELECT candidates.*, parties.name AS party_name
-    FROM candidates
-    LEFT JOIN parties
-    ON candidates.party_id = parties.id`;
-    const params = []
-    db.all(sql, params, (err, rows) => {
-        if(err) {
-            res.status(500).json({error: err.message});
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: rows
-        });
-      });
-});
-
-
-// create a query for read operation
-// GET a single candidate
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-    AS party_name 
-    FROM candidates 
-    LEFT JOIN parties 
-    ON candidates.party_id = parties.id 
-    WHERE candidates.id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-        if(err) {
-          res.status(400).json({error: err.message});
-          return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-      });
-})
-
-
-//create a query for delete operation
-// Delete a candidate; ? mark denotes a placeholder making this a prepared statement
-app.delete("/api/candidate/:id", (req,res)=>{
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id]
-
-    db.run(sql, params, function(err, result) {
-        if (err) {
-         res.status(404).json({error: res.message});
-         return;
-        }
-    
-        res.json({
-            message: 'successfully deleted',
-            changes: this.changes
-            //this.changes verifies if any rows were deleted
-        });
-    });
-});
-
-
-//create a query for create operation
-// Create a candidate
-
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-      res.status(400).json({ error: errors });
-      return;
-    }
-
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-              VALUES (?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-    // ES5 function, not arrow function, to use `this`
-    db.run(sql, params, function(err, result) {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-
-        res.json({
-            message: 'success',
-            data: body,
-            id: this.lastID
-        });
-    });
-});
-
-//route that handles updates to a candidates party affiliation
-app.put('/api/candidate/:id', (req, res) => {
-    //beginning error check makes sure the party id was provided before attempt to update the database.
-    //forces a the request to include a party id property
-    const errors = inputCheck(req.body, 'party_id');
-
-    if (errors) {
-    res.status(400).json({ error: errors });
-    return;
-    }
-    
-    const sql = `UPDATE candidates SET party_id = ? 
-     WHERE id = ?`;
-    const params = [req.body.party_id, req.params.id];
-  
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: req.body,
-        changes: this.changes
-      });
-    });
-});
-
-//route to get all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-});
-
-//get route to get a single party 
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-});
-
-//route to be able to delete a party 
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        return;
-      }
-  
-      res.json({ message: 'successfully deleted', changes: this.changes });
-    });
-});
-
+//Use api routes
+app.use('/api', apiRoutes);
 
 // Default response for any other request(Not Found) Catch all
   // route to handle user requests that aren't supported by the app,
 app.use((req, res) => {
     res.status(404).end();
-  });
+});
 
-  //Ensure Express.js(server) doesnt start before the connecttion to database has been estabished by wrapping the express.server connection in an event handler.
+//Ensure Express.js(server) doesnt start before the connecttion to database has been estabished by wrapping the express.server connection in an event handler.
   db.on('open', () => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  });
+});
